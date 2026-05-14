7fe6b015a58a71e151c0261126853a3c28818931
musaSaadatyar <musa.saadatyar@email.com>
2026-05-14 13:35:37 +0330

    save data

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
-const mongoose = require("mongose");
+const mongoose = require("mongoose");
 
 /**
- *1: connected to mongo db
+ * 1: connect to mongo db
  */
 mongoose
   .connect("mongodb://localhost/firstDb")
-  .then(() => console.log("conected to mongo db"))
+  .then(() => console.log("connected to mongo db"))
   .catch((error) => console.log(error));
 
 /**
- * 2: creat schima and data
+ * 2: create schema and data
  */
 const courseSchema = new mongoose.Schema({
   name: String,
-  auther: String,
-  tages: [String],
+  author: String,
+  tags: [String],
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
 });
 
 const Course = mongoose.model("Course", courseSchema);
 
-const course = new Course({
-  name: "moosa",
-  auther: "saadatyar",
-  tages: ["angular", "node.js", "npm"],
-  isPublished: true,
-});
+async function createCourse() {
+  const course = new Course({
+    name: "donia",
+    author: "mohamadi",
+    tags: ["angular", "js", "npm"],
+    isPublished: true,
+  });
+
+  /**
+   * 3: save data
+   */
+  const result = await course.save();
+  console.log(result);
+}
+
+createCourse();
