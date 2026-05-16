4cd1f295e39acbcf1eee11727773ecfca9318bdf
musaSaadatyar <musa.saadatyar@email.com>
2026-05-16 19:59:39 +0330

    46

--- index.js
+++ index.js
@@ -49,32 +49,111 @@ async function getCoursess() {
   /**
    * 5 use or and
    */
-  const courseDb2 = await Course.find()
-    .or([{ author: "mohamadi" }, { name: "moosa" }])
-    .and([{ author: "mohamadi" }, { name: "moosa" }]);
+  // const courseDb2 = await Course.find()
+  //   .or([{ author: "mohamadi" }, { name: "moosa" }])
+  //   .and([{ author: "mohamadi" }, { name: "moosa" }]);
 
-/**
- * Regular Expressions (Regex) in MongoDB
- *
- * starts with:
- * /^name/
- *
- * ends with:
- * /name$/
- *
- * case insensitive:
- * /name$/i
- *
- * contains:
- * /.*name.*/
-  
- */
+  /**
+   * Regular Expressions (Regex) in MongoDB
+   *
+   * starts with:
+   * /^name/
+   *
+   * ends with:
+   * /name$/
+   *
+   * case insensitive:
+   * /name$/i
+   *
+   * contains:
+   * /.*name.*/
 
-   
-  const regularDb = await Course.find({ author: /di$/ });
+  // const regularDb = await Course.find({ author: /di$/ });
 
-  
-  console.log(regularDb);
+  /**
+   * .count() => return count result
+   * skip(2) => dont show 2 data
+   * countDocuments()  retern total document
+   */
+
+  const pageNaumber = 2;
+  const pageSize = 10;
+  const result48 = await Course.find()
+    .limit(pageSize)
+    .sort({ name: 1 })
+    .skip((pagenumber - 1) * pageSize)
+    .countDocuments();
+
+  console.log(result48);
+  getCoursess();
 }
 
-getCoursess();
+async function updateCourse(id) {
+  const course = await Course.findById(id);
+  if (!course) return;
+
+  //one way
+  // course.isPublished = false;
+  // course.author = " hasan hasani";
+
+  //towe way
+  course.set({
+    isPublished: false,
+    author: "hasan hasani",
+  });
+  const resoult = await course.save();
+  console.log("resoult", resoult);
+  updateCourse("6a059d1f12472c55130eae74");
+}
+
+async function updateCourseById(id) {
+  const course = await Course.findById(id);
+  if (!course) return;
+
+  //one way
+  // course.isPublished = false;
+  // course.author = " hasan hasani";
+
+  //towe way
+  course.set({
+    isPublished: false,
+    author: "hasan hasani",
+  });
+  const resoult = await course.save();
+  console.log("resoult", resoult);
+}
+
+async function updateFirst(id) {
+  const course = await Course.updateOne(
+    { _id: id },
+    {
+      $set: {
+        isPublished: false,
+        author: "hasan hasi",
+      },
+    },
+  );
+
+  const course2 = await Course.findByIdAndUpdate(
+    { _id: id },
+    {
+      $set: {
+        isPublished: false,
+        author: "hasan 258",
+      },
+    },
+    { new: true },
+  );
+
+  console.log("resoult", course2);
+  updateFirst("6a059d1f12472c55130eae74");
+}
+
+const removeCourse = async (id) => {
+  // const resulte = await Course.deleteOne({ _id: id });
+  // const resulte = await Course.findByIdAndDelete({ _id: id });
+  const resulte = await Course.deleteMany({ isPublished: true });
+
+  console.log(resulte);
+};
+removeCourse("6a059d1f12472c55130eae74");
